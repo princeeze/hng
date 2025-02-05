c81a08843aaabe8f515dd96b4f384aa09e5e776f
Prince Eze <pchidireeze@gmail.com>
2025-02-05 14:19:11 +0100

    feat: add home page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,17 @@
-import { Stack } from "expo-router";
-import "expo-dev-client";
+import { Link } from "expo-router";
+import { View, Text, Button, Alert } from "react-native";
+import tw from "twrnc";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#f4511e",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <View style={tw`justify-center items-center gap-8 w-full h-full`}>
+      <Text style={tw`text-2xl`}>HNG Task 0</Text>
+      <Link
+        style={tw`p-4 text-white bg-blue-500 rounded`}
+        href="https://github.com/princeeze/hng"
+      >
+        Open Repo
+      </Link>
+    </View>
   );
 }
