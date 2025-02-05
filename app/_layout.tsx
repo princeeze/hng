f58665c990f544707cb757347df97ca1ecbaa91a
Prince Eze <pchidireeze@gmail.com>
2025-02-05 14:48:24 +0100

    feat: create layout with styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,12 +6,22 @@ export default function RootLayout() {
   return (
     <View style={tw`justify-center items-center gap-8 w-full h-full`}>
       <Text style={tw`text-2xl`}>HNG Task 0</Text>
-      <Link
-        style={tw`p-4 text-white bg-blue-500 rounded`}
-        href="https://github.com/princeeze/hng"
-      >
-        Open Repo
-      </Link>
+      <View style={tw` flex-row gap-4 w-full px-4`}>
+        <View style={tw`p-4 justify-center items-center bg-blue-500 rounded`}>
+          <Link
+            style={tw`text-white hover:bg-opaci`}
+            href="https://github.com/princeeze/hng"
+          >
+            Open Repo
+          </Link>
+        </View>
+        <Link
+          style={tw`p-4 text-white bg-blue-500 rounded text-center flex-1`}
+          href="https://hng.tech/hire/react-native-developers"
+        >
+          React Native Developers
+        </Link>
+      </View>
     </View>
   );
 }
